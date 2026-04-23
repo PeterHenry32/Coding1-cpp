8327ac83fe0b76cd51cce124bbbd09e3904b9665
PeterHenry32 <Henryphmcfc@gmail.com>
2026-04-23 15:18:50 -0500

    Update favs.cpp

--- week 3/favs.cpp
+++ week 3/favs.cpp
@@ -9,21 +9,45 @@
     using namespace std;
 
     int main() {
+       
+        vector<string> favs;
+
         while(true) {
             cout << "What would you like to do/\n";
             cout << "You can 'add', 'edit', 'remove', 'show', or 'quit'.\n";
 
-            string input
-            getlilne(cin, input);
+            string input;
+            getline(cin, input);
             
             if(input == "quit") {
                 cout << "Thanks for playing.\n";
                 break;
             }
             else if(input == "add") {
-                ///
+               //ask them for input
+               cout << "What game would you like to add?\n";
+               //get input
+               getline(cin, input);
+               //add the game to the list 
+               favs.push_back(input);
             }
-            else{
+            else if(input == "edit") {
+                //show them list of games
+                cout << "here are your friends:\n";
+            for(int i = 0; i < favs.size(); i++) {
+                cout << favs[i] << ".\n";
+            }  
+            }
+
+            else if(input == "show") {
+                //show them input
+                cout << "Here are your favorite games!\n";
+                for(vector<string>::iterator iter = favs.begin(); iter != favs.end(); iter++) {
+                    cout << *iter << endl;
+                }   
+            }
+
+            else {
                 cout << "I dont recognize that input.\n";
             }
         }
